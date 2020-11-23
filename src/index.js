5fc42ac2ace4f41824b2d6ccf952bbe17310ad89
LebsterFace <samsalinsky@gmail.com>
2020-11-23 01:56:28 +0000

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,58 +1,14 @@
 //#region Setup
-const instructions = require("./instructions"),
-	readline = require("readline"),
-	memory = new Uint16Array(65536),
+
+const instructions = require("./instructions");
+
+const memory = new Uint16Array(65536),
 	registerNames = ["ip", "acc", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "sp"],
 	registerMap = registerNames.reduce((map, cv, index) => {
 		map[cv] = index;
 		return map;
 	}, {}),
-	registers = new Uint16Array(registerNames.length),
-	stackLength = 128;
-
-reg("sp", 0xffff - stackLength);
-
-//#endregion
-//#region Debugging
-
-function memoryPeek(from = 0, prepend = false) {
-	console.log(`${prepend ? "\n> " : ""}${formatValue(from)}: [${formatNumber(memory[from])}] ${Array.from(memory.slice(from + 1, from + 4)).map(formatNumber).join(" ")}`);
-}
-
-function memoryPeekAround(around, prepend = false) {
-	const before = Array.from(memory.slice(around - 2, around)).map(formatNumber),
-		middle = "[" + formatNumber(memory[around]) + "]",
-		end = Array.from(memory.slice(around + 1, around + 3)).map(formatNumber);
-
-	console.log(`${prepend ? "\n> " : ""}${formatValue(around)}! ${before.concat(middle).concat(end).join(" ")}`);
-}
-
-function formatValue(n) {
-	return n.toString(16).padStart(4, "0").toUpperCase();
-}
-
-function formatNumber(n) {
-	return "0x" + n.toString(16).toUpperCase().padStart(2, "0");
-}
-
-const INSTRUCTION_NAMES = {};
-for (const name in instructions) INSTRUCTION_NAMES[instructions[name]] = name;
-
-function getInstructionName(value) {
-	return INSTRUCTION_NAMES[value];
-}
-
-function debug() {
-	console.log(`\nNext Instruction : ${getInstructionName(memory[reg("ip")])}`);
-	memoryPeek(reg("ip"));
-	memoryPeekAround(reg("sp"));
-
-	registerNames.forEach((name, index) => {
-		console.log(`${name.padEnd(4, " ")}: ${formatValue(registers[index])}`);
-	});
-
-	console.log();
-}
+	registers = new Uint16Array(registerNames.length);
 
 //#endregion
 //#region Helper
@@ -75,6 +31,44 @@ function increg(name, amount = 1) {
 	const result = registers[registerMap[name]];
 	registers[registerMap[name]] += amount;
 	return result;
+}
+
+//#endregion
+//#region Debugging
+
+const INSTRUCTION_NAMES = {};
+for (const name in instructions) INSTRUCTION_NAMES[instructions[name]] = name;
+
+function memoryPeek(from = 0, prepend = false) {
+	console.log(`${prepend ? "\n> " : ""}${formatValue(from)}: [${formatNumber(memory[from])}] ${Array.from(memory.slice(from + 1, from + 4)).map(formatNumber).join(" ")}`);
+}
+
+function memoryPeekAround(around, prepend = false) {
+	const before = Array.from(memory.slice(around - 2, around)).map(formatNumber),
+		middle = "[" + formatNumber(memory[around]) + "]",
+		end = Array.from(memory.slice(around + 1, around + 3)).map(formatNumber);
+
+	console.log(`${prepend ? "\n> " : ""}${formatValue(around)}! ${before.concat(middle).concat(end).join(" ")}`);
+}
+
+function formatValue(n) {
+	return n.toString(16).padStart(4, "0").toUpperCase();
+}
+
+function formatNumber(n) {
+	return "$" + n.toString(16).toUpperCase().padStart(2, "0");
+}
+
+function debug() {
+	console.log(`\nNext Instruction : ${INSTRUCTION_NAMES[memory[reg("ip")]]}`);
+	memoryPeek(reg("ip"));
+	memoryPeekAround(reg("sp"));
+
+	registerNames.forEach((name, index) => {
+		console.log(`${name.padEnd(4, " ")}: ${formatValue(registers[index])}`);
+	});
+
+	console.log();
 }
 
 //#endregion
@@ -171,40 +165,18 @@ function execute(instruction) {
 			reg(register, memory[increg("sp", -1) - 1]);
 			return true;
 		}
-
-		// DEBUG INSTRUCTIONS
-
-		case instructions.DEBUG_REG: {
-			const regName = getRegister();
-			const value = reg(regName);
-			console.log(`> ${regName.padStart(4, " ")} : ${formatNumber(value)}`);
-			return true;
-		}
-		case instructions.PEEK_MEM: {
-			memoryPeek(fetch(), true);
-			return true;
-		}
 	}
 }
 
-function step() {
-	return execute(fetch());
-}
+reg("sp", 0xfeff);
 
 //#endregion
 
-const prog = [0x01, 0x69, 0x00, 0x0a, 0x00, 0x0b, 0x01, 0xffff];
+const prog = [
+	instructions.MOV_LIT_MEM, 0x69, 0x00,
+	instructions.ADD_LIT_REG, 0x01, 0x00
+];
 
 for (const i in prog) memory[i] += prog[i];
-
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-
-var keepGoing = true;
-rl.on("line", () => {
-	if (!keepGoing) process.exit(0);
-	debug();
-	keepGoing = step();
-});
+debug();
+while (execute(fetch())) debug();
---
+++ src/instructions.js
@@ -0,0 +1,15 @@
+module.exports = {
+	MOV_LIT_REG : 0x01,
+	MOV_REG_REG : 0x02,
+	MOV_REG_MEM : 0x03,
+	MOV_MEM_REG : 0x04,
+	MOV_LIT_MEM : 0x05,
+	ADD_REG_REG : 0x06,
+	ADD_LIT_REG : 0x07,
+	JMP_NOT_EQ  : 0x08,
+	PUSH_LIT    : 0x09,
+	PUSH_REG    : 0x0A,
+	PULL_REG    : 0x0B,
+	PULL_ME     : 0x0C,
+	HALT        : 0xffff
+};
